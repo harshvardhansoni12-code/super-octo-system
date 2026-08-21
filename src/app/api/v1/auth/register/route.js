a1b9b712cc230a61f5f3130e42b8a9c83b2ed2e8
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-08-21 19:10:01 +0530

    register route is working

--- src/app/api/v1/auth/register/route.js
+++ src/app/api/v1/auth/register/route.js
@@ -1,97 +1,13 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
-import bcrypt from "bcryptjs";
-import crypto from "crypto";
-import { jwt } from "jsonwebtoken";
-import { cookies } from "next/headers";
+import { registerUser } from "@/services/user/user-service";
 
 export async function POST(request) {
   try {
-    const { email, password, name } = await request.json();
-
-    //no email or password check
-    if (!email || !password) {
-      return NextResponse.json(
-        { error: "Email and password are required" },
-        { status: 400 },
-      );
-    }
-
-    //password length check
-    if (password.length < 6) {
-      return NextResponse.json(
-        {
-          error: "Password should be atleast 6 characters long",
-        },
-        { status: 400 },
-      );
-    }
-
-    //existing user check
-    const userExists = await prisma.user.findUnique({
-      where: { email },
-    });
-
-    if (userExists) {
-      return NextResponse.json(
-        { error: "User with this email already exists" },
-        {
-          status: 409,
-        },
-      );
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-      },
-    });
-    const tokenvalue = crypto.randomBytes(40).toString("hex");
-    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); //7days
-    const refresh_token = await prisma.refresh_token.create({
-      data: {
-        token: tokenvalue,
-        revoked: false,
-        expiresAt,
-        user: {
-          connect: { id: newUser.id },
-        },
-      },
-    });
-    const jwt_token = await jwt.sign(newUser.id, process.env.SECRET_KEY);
-    const cookieStore = await cookies();
-    cookieStore.set("auth_token", jwt_token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 60 * 60 * 24, // one day
-      path: "/",
-    });
-    cookieStore.set("refresh_token", refresh_token.token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 60 * 60 * 24 * 7, // one week
-      path: "/",
-    });
-
-    const { password: _password, ...userwithoutPassword } = newUser;
-    return NextResponse.json(
-      {
-        message: "User created Succesfully",
-        user: userwithoutPassword,
-      },
-      { status: 201 },
-    );
+    return await registerUser(request);
   } catch (error) {
-    console.log("Register Error", error);
     return NextResponse.json(
       {
-        error: "Something went wrong",
+        error: error.message || "Something went wrong",
       },
       { status: 500 },
     );
